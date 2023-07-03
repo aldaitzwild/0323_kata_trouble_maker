d09670f03b90385e218a647310f67afa7c349111
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-07-03 08:50:18 +0200

    Initiate the new kata

---
+++ kata.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * La fonction hackify doit prend n'importe quel chaine de caractère et :
+ *  - Remplacer les 'l' ou 'L' par des '!'
+ *  - Remplacer les 's' ou 'S' par des 5
+ *  - Remplacer toutes les voyelles par leur code ASCII
+ */
+
+function hackify(string $passPhrase) : string
+{
+    return '';
+}
